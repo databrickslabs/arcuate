65874adce96489bb07353c607ad66d9fd91fb77b
nkvuong@gmail.com <nkvuong@gmail.com>
2022-01-31 13:40:37 +0000

    minor cleanup

--- 03-MagicArcuate.py
+++ 03-MagicArcuate.py
@@ -12,20 +12,20 @@ from sqlparse.tokens import Whitespace
 from typing import List
 
 def arcuate_parse(query:str) -> List[str]:
-    query = query.replace(' MODEL ', ' MODE ')
-    tokens = [item.value.upper() for item in sqlparse.parse(query)[0] if item.ttype != Whitespace]
+    query = query.upper().replace(' EXPERIMENT ', ' MODE ')
+    tokens = [item.value for item in sqlparse.parse(query)[0] if item.ttype != Whitespace]
     if tokens[0]!= 'CREATE' or tokens[1] not in ['SHARE', 'MODE'] or tokens[3] != 'AS' or tokens[4] != 'SELECT':
         raise NotImplementedError("syntax not supported")
 
     id = tokens[2]
-    return [share_id, tokens[5:]]
+    return [id, tokens[5:]]
 
 # COMMAND ----------
 
-query = 'create share `share  id` as select model model_id'
+query = 'create share `shr  s123` as select experiment `exp e123`'
 arcuate_parse(query)
 
 # COMMAND ----------
 
-query = 'create MODEL model_id as select * from share share_id'
+query = 'create EXPERIMENT `exp e123` as select * from `shr s123`'
 arcuate_parse(query)
