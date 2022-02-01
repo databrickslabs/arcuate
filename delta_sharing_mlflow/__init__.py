494a3174db8b08725ef6b124fd4045cb58041106
Vuong Nguyen <44292934+nkvuong@users.noreply.github.com>
2022-02-01 16:30:17 +0000

    update init

--- delta_sharing_mlflow/__init__.py
+++ delta_sharing_mlflow/__init__.py
@@ -1,6 +1,7 @@
 from delta_sharing_mlflow.provider import pickle_model_udf, pickle_artifacts_udf, normalize_mlflow_df, export_models
 from delta_sharing_mlflow.recipient import write_and_log_artifacts, chunks, import_models
 from delta_sharing_mlflow.parser import arcuate_parse
+from delta_sharing_mlflow.magic import ArcuateMagic
 from delta_sharing_mlflow.version import __version__
 
 __all__ = [
